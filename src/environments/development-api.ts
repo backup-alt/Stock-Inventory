833f6bf11f524ee822819327e3a1079b416aee54
backup-alt <universeexplorer4@gmail.com>
2026-05-25 20:35:03 +0530

    Use direct report APIs in app

--- src/environments/development-api.ts
+++ src/environments/development-api.ts
@@ -1,4 +1,6 @@
 export const developmentApi = {
-  baseUrl: 'http://127.0.0.1:8080',
-  key: 'lf_live_0Cg3N7UnBM7usWDsE4vyhuKOfQGpKOEund03oMC6U6g',
+  baseUrl: 'https://ktm-salt.onrender.com',
+  summaryReportsUrl: 'https://ktm-salt.onrender.com/reports/getSummaryReports',
+  stockReportsUrl: 'https://ktm-salt.onrender.com/reports/getStockReports',
+  key: '',
 };
